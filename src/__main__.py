7995fb7e09d9eea301bc326fc7cfcbc2f5f6ab7c
MuhammadTofikHidayat <topikhidayat17081998@gmail.com>
2024-02-23 07:59:00 +0700

    selesai membuat fitur show dan add

---
+++ src/__main__.py
@@ -0,0 +1,95 @@
+import function
+import csv
+import sys
+import os
+
+def initialize_db():
+         """
+         Funsi ini berfungsi sebagai inisialisasi data yang diambil dari db
+         """
+
+         with open (PATH, 'r') as file:
+                  reader = csv.reader(file, delimiter=';')
+                  database = {}
+                  for row in reader:
+                           no,kode_barang,nama,harga,stok,keterangan = row
+                           database.update({nama: [int(no), kode_barang,nama, int(harga), int(stok), keterangan]})
+
+         return database
+
+def main():
+         """
+         Main Program utama
+         """
+
+         while True : 
+                  #inputan dari user
+                  choice = function.int_validation(main_menu)
+                  function.clear_screen()
+                  if choice == 1 :
+                           function.show_menu_data(database)
+                           function.clear_screen()
+                  elif choice == 2 :
+                           function.add_data_barang(database)
+                           function.clear_screen()
+                  elif choice == 5:
+                           print(f'Terimakasih....{username} sampai jumpa kembali\n')
+                           break
+                  else:
+                           print('Masukan angka 1 sampai 5 !')
+                           continue
+
+                  # Menjaga agar database selalu diperbarui
+         with open(PATH, 'w') as file:
+                  # Membuat objek writer
+                  writer = csv.writer(file, delimiter=";")
+                  # Menulis data ke dalam file csv
+                  writer.writerows(database.values())
+
+
+
+if __name__ == "__main__":
+
+                  #mendefinisikan menu utama aplikasi
+                  main_menu = '''
+                  SELAMAT DATANG DI TOKO BERKAH JAYA
+
+Menu Utama : 
+
+1. Menampilkan Data Barang
+2. Menambahkan Data Barang
+3. Menghapus Data Barang
+4. Cek Out Barang 
+5. Exit
+
+Silahkan Pilih Angka 1 sampai 5 : '''
+
+                  if getattr(sys, 'frozen', False):
+                           PATH = sys._MEIPASS
+                           PATH = os.path.join(PATH, 'data/db.csv') 
+                  else:
+                           PATH = os.getcwd()
+                           PATH = os.path.join(PATH, 'data/db.csv') 
+
+         # Membersihkan tampilan user
+function.clear_screen()
+
+         # Initializing database
+database = initialize_db()
+
+while True :
+         print("""
+                           LOGIN APP TOKO BERKAH JAYA
+""")
+         username = function.str_validation('Masukan username : ')
+         password = function.str_validation('Masukan password : ')
+
+         if username.lower() == 'topik' and password.lower() == '123':
+                  #menjalankan menu utama
+                  function.clear_screen()
+                  print(f'Berhasil login.... Selamat datang {username}')
+                  main()
+         else:
+                  function.clear_screen()
+                  print('Username dan Password salah !\n')
+                  continue
---
+++ src/function.py
@@ -0,0 +1,179 @@
+from tabulate import tabulate
+import os
+
+
+def clear_screen():
+         """
+         A function to clean the user interface
+         """
+         # For Windows
+         if os.name == 'nt':
+                  _ = os.system('cls')
+         # For macOS and Linux
+         else:
+                  _ = os.system('clear')
+
+def int_validation(prompt):
+         """
+         Fungsi untuk melakukan validasi input bilangan bulat positif.
+         """
+
+         while True:
+                  try:
+                           integer = int(input(prompt))
+                           if integer < 0:
+                                    print("\nPeringatan : masukan anda tidak boleh negatif !")
+                                    clear_screen()
+                                    continue
+                           else:
+                                    return integer
+                  except ValueError:
+                                    clear_screen()
+                                    print("\nPeringatan : Silakan masukan bilangan bulat !")
+
+
+def str_validation(str):
+         """
+         Fungsi untuk melakukan validasi inputan str atau bukan
+         """
+
+         while True : 
+                  setense = input(str)
+                  if setense.isalnum() or setense.replace(" ", "").upper():
+                           return setense
+                  else:
+                           print('Peringatan : Silahkan input alfabet yang benar!')
+                           continue
+
+
+def data_table(data, coloums, title):
+         """
+         Fungsi untuk membuat sebuah tabel dengan library tabulate
+         """
+         print(title.center(80))
+         print(tabulate(data, headers=coloums, tablefmt='grid'))
+
+def show_menu_data(database):
+         #variabel menu read data
+         read_menu_data = '''
+                  SELAMAT DATANG DI TOKO BERKAH JAYA
+
+Menu Read Data : 
+
+1. Tampilkan Data Barang
+2. Cari Data Barang
+3. Kembali Menu Utama
+
+Silahkan Pilih Angka 1 sampai 3 : '''
+                  
+         while True:
+                  choice_read = int_validation(read_menu_data)
+                  clear_screen()
+                  if choice_read == 1:   
+                           data_table(
+                           data=database.values(),
+                           coloums=['No', 'Kode Barang', 'Nama Barang','Harga', 'Stok' , 'Keterangan'],
+                           title='\nTABEL BARANG TOKO BERKAH JAYA\n'         
+                           )
+                  elif choice_read == 2:
+                           data_table(
+                           data=database.values(),
+                           coloums=['No', 'Kode Barang', 'Nama Barang','Harga', 'Stok' , 'Keterangan'],
+                           title='\nTABEL BARANG TOKO BERKAH JAYA\n'         
+                           ) 
+                           show_data_filter(database)
+                  elif choice_read == 3 :
+                           break
+                  else :
+                           print('Masukan angka 1 sampai 3 !')
+                           continue
+
+def show_data_filter(database):
+         
+         while True:
+                  kode_barang = str_validation('\nMasukan kode barang : ')
+                  clear_screen()
+                  for item in database.values():
+                           if kode_barang.upper() == item[1]:
+                                    data_table(
+                                             data=[item],
+                                             coloums=['No', 'Kode Barang', 'Nama Barang', 'Harga', 'Stok', 'Keterangan'],
+                                             title='\nTABEL BARANG TOKO BERKAH JAYA\n'
+                                    )
+                                    print('\nBarang yang Anda cari telah ditemukan.\n')
+                                    return  # Berhenti dari fungsi setelah menampilkan data
+                  else:
+                           print('\nMohon maaf kode barang yang Anda cari tidak ada, mohon priksa kembali kode barang.')
+                           break
+
+
+def add_data_barang(database):
+         """
+         Fungsi untuk menambahkan data barang
+         """
+         #variabel menu add data barang
+         read_add_data = '''
+                  SELAMAT DATANG DI TOKO BERKAH JAYA
+
+Menu Tambah Data : 
+
+1. Tambahkan Data Barang
+2. Kembali Menu Utama
+
+Silahkan Pilih Angka 1 sampai 2 : '''
+
+         while True:
+                  choice_add = int_validation(read_add_data)
+                  clear_screen()  
+                  if choice_add == 1:
+                           while True:
+                                    print("""\n                     MENAMBAHKAN DATA BARANG         \n""")
+                                    kode_barang = str_validation('Masukan Kode Barang : ')
+                                    # Validasi panjang kode barang dan awalan "KB"
+                                    if len(kode_barang) != 5 or not kode_barang.startswith("KB"):
+                                             print("Kode barang harus memiliki panjang 5 karakter dan diawali dengan 'KB'. Contoh : 'KB999'")
+                                             continue
+
+                                    items = list(database.values())
+                                    #meriksa apakah kode barang sudah ada atau belum                 
+                                    for item in items:
+                                             if kode_barang.upper() == item[1]:
+                                                      print('Mohon maaf kode barang yang anda masukan sudah terdaftar.')
+                                                      break
+                                             else:    
+                                                      nama_barang = str_validation('Masukan Nama Barang : ')
+                                                      harga = int_validation('Masukan Harga Barang : ')
+                                                      stok = int_validation('Masukan Stok Barang : ')
+                                                      keterangan = str_validation('Masukan Keterangan : ')
+
+                                             validasi_save = str_validation('\nAnda ingin save (y/t) : ')
+                                             if validasi_save.lower() == 'y' :
+                                                      database.update({
+                                                               f'{nama_barang}': [
+                                                                        len(database)+1,
+                                                                        kode_barang.upper(),
+                                                                        nama_barang,
+                                                                        harga,
+                                                                        stok,
+                                                                        keterangan]
+                                                                        })
+                                                      data_table(
+                                                      data=database.values(),
+                                                      coloums=['No', 'Kode Barang', 'Nama Barang','Harga', 'Stok' , 'Keterangan'],
+                                                      title='\nTABEL BARANG TOKO BERKAH JAYA\n'         
+                                                      )
+                                                      print('\nData Berhasil Disimpan.')
+                                                      break
+                                             else:
+                                                      data_table(
+                                                      data=database.values(),
+                                                      coloums=['No', 'Kode Barang', 'Nama Barang','Harga', 'Stok' , 'Keterangan'],
+                                                      title='\nTABEL BARANG TOKO BERKAH JAYA\n'         
+                                                      )
+                                                      break
+                                    break
+                  elif choice_add == 2 :
+                           break
+                  else:
+                           print('Masukan angka 1 sampai 2 !')
+                           continue
